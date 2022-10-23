0bf5b1bc8dca4d63362eb703abb1b1dbf40e909f
kaisar-barlybay <kaisar.barlybay.sse@gmail.com>
2022-10-23 10:19:18 +0600

    sharpen + grayscale

--- app.py
+++ app.py
@@ -13,23 +13,30 @@ cors = CORS(app)
 @app.route("/apply_filter", methods=["POST"])
 def apply_filter():
   input_image = request.files['image']
-  img = cv2.imdecode(np.asarray(bytearray(input_image.stream.read()), dtype=np.uint8), cv2.IMREAD_COLOR)
+  img: ndarray = cv2.imdecode(np.asarray(bytearray(input_image.stream.read()), dtype=np.uint8), cv2.IMREAD_COLOR)
   filter_type = request.form.get('filter_type').lower()
+  amount_str = request.form.get('amount')
+  amount = int(amount_str) if amount_str != None else 1
+  print(f'{img.shape}')
 
   if filter_type == 'gaussian_blur':
     pass
-  elif filter_type == 'greyscale':
-    pass
-  elif filter_type == 'invert':
-    kernel = np.array([[-1, -1, -1], [-1, 9.5, -1], [-1, -1, -1]])
+  elif filter_type == 'grayscale':
+    # channel_ndarray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    channel_ndarray = np.dot(img[..., :3], [0.2989, 0.5870, 0.1140])
+  elif filter_type == 'sharpen':
+    basic_kernel = np.array([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
+    sharpen_kernel = np.array([[0, -1, 0], [-1, 4, -1], [0, -1, 0]]) * amount
+    kernel = basic_kernel + sharpen_kernel
     channel_ndarray: ndarray = cv2.filter2D(img, -1, kernel)
   else:
     return
 
+  print(f'{channel_ndarray.shape}')
   # channel_ndarray should be 3 channel
   encoded, plain_ndarray = cv2.imencode('.png', channel_ndarray)
   encoded, plain_ndarray = cast(tuple[bool, ndarray], (encoded, plain_ndarray))
-  return send_file(BytesIO(plain_ndarray.tobytes()), mimetype='image/gif')
+  return send_file(BytesIO(plain_ndarray.tobytes()), mimetype='image/gif', download_name='out.png')
 
 
 if __name__ == "__main__":
